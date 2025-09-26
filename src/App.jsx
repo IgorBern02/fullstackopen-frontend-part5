b92957013bc21a988639569380f122b2a9799665
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-09-26 18:04:00 -0300

    feat: add exercise 5.3 from part 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import Login from "./components/Login";
 import loginService from "./services/login,";
+import CreateBlog from "./components/CreateBlog";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -80,8 +81,14 @@ const App = () => {
       ) : (
         <>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+
+          <span>{user.name} logged in</span>
           <button onClick={handleLogout}>logout</button>
+
+          <div>
+            <h2>create new</h2>
+            <CreateBlog createBlog={blogService.create} setBlogs={setBlogs} />
+          </div>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
