7416a78226b99bf6b001583eaaad2d9c0f82f8d1
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-10-01 18:46:32 -0300

    feat: add exercise 5.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import Login from "./components/Login";
 import loginService from "./services/login";
 import CreateBlog from "./components/CreateBlog";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,7 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
+  const blogFormRef = useRef();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -51,6 +53,8 @@ const App = () => {
         setErrorMessage(null);
       }, 5000);
     }
+
+    blogFormRef.current.toggleVisibility();
   };
 
   useEffect(() => {
@@ -83,33 +87,35 @@ const App = () => {
   return (
     <div>
       {user === null ? (
-        <>
-          <h2>log in to application</h2>
+        <Togglable buttonLabel="login">
           <Login
+            username={username}
+            password={password}
             handleLogin={handleLogin}
             setUsername={setUsername}
             setPassword={setPassword}
           />
-        </>
+        </Togglable>
       ) : (
-        <>
-          <h2>blogs</h2>
-
-          {successMessage && <div className="success">{successMessage}</div>}
-          {errorMessage && <div className="error">{errorMessage}</div>}
-
-          <span>{user.name} logged in</span>
-          <button onClick={handleLogout}>logout</button>
-
-          <div>
-            <h2>create new</h2>
-            <CreateBlog createBlog={addBlog} />
-          </div>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
-        </>
+        <div>
+          <p>
+            {user.name} logged in <button onClick={handleLogout}>logout</button>
+          </p>
+        </div>
       )}
+
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+      {successMessage && <div style={{ color: "green" }}>{successMessage}</div>}
+
+      <h2>Blogs</h2>
+
+      <Togglable buttonLabel="new blog" ref={blogFormRef}>
+        <CreateBlog createBlog={addBlog} />
+      </Togglable>
+
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 };
