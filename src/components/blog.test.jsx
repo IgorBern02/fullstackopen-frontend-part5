03a71f52192f00bd8c60bf8f418b4bc5b1daf589
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-11-03 15:39:56 -0300

    feat: add exercise 5.14

--- src/components/blog.test.jsx
+++ src/components/blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Blog from "./Blog";
+import { test } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 test("renders title and author, but not url or likes by default", () => {
   const blog = {
@@ -24,4 +26,24 @@ test("renders title and author, but not url or likes by default", () => {
   // não deve mostrar url nem likes
   expect(screen.queryByText("https://react.dev")).toBeNull();
   expect(screen.queryByText("likes 10")).toBeNull();
+});
+
+test("shows URL and number of likes after clicking the view button", async () => {
+  const blog = {
+    title: "Aprendendo React",
+    author: "Igor",
+    url: "https://react.dev",
+    likes: 10,
+    user: { username: "igor" },
+  };
+
+  render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  // Agora sim: verificar se aparecem na tela
+  expect(screen.getByText("https://react.dev")).toBeInTheDocument();
+  expect(screen.getByText("likes 10")).toBeInTheDocument();
 });
